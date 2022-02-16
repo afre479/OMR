49bd85c91551325204e1567c6d3e3bfb86d22392
conno <csteigerwald248@student.fuhsd.org>
2022-02-16 10:41:38 -0800

    item analysis

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 // Author: David Dobervich (this is my edit)
 // ANOTHER EDIT.
 public class OpticalMarkReaderMain {
-    private static int numStudentPages=2;
+    private static int numStudentPages = 2;
 
     public static void main(String[] args) {
         String pathToPdf = fileChooser();
@@ -19,63 +19,64 @@ public class OpticalMarkReaderMain {
         System.out.println("Loading pdf at " + pathToPdf);
 
         PageResult answerKey = setFilter(0, pathToPdf);
-        for (int i = 1; i < numStudentPages+1; i++) {
+        for (int i = 1; i < numStudentPages + 1; i++) {
             studentAnswers[i - 1] = setFilter(i, pathToPdf);
         }
-        String data=gradeTest(studentAnswers,answerKey);
-        PDFHelper.writeDataToFile("answers.csv",data);
-        String analysis=itemAnalysis(studentAnswers,answerKey);
-        PDFHelper.writeDataToFile("itemAnalysis.csv",analysis);
+        String data = gradeTest(studentAnswers, answerKey);
+        PDFHelper.writeDataToFile("answers.csv", data);
+        String analysis = itemAnalysis(studentAnswers, answerKey);
+        PDFHelper.writeDataToFile("itemAnalysis.csv", analysis);
     }
 
-    public static String itemAnalysis(PageResult[] studentAnswers, PageResult answerKey){
-        String data="";
-        ArrayList<String>correctAnswers=answerKey.getAnswers();
-        ArrayList<String>questions=studentAnswers[0].getAnswers();
-        for (int i = 1; i < questions.size()+1; i++) {
-            int count=0;
-            for (PageResult student:studentAnswers) {
-                ArrayList<String>answers=student.getAnswers();
-                if(answers.get(i-1).equals(correctAnswers.get(i-1))){
+    public static String itemAnalysis(PageResult[] studentAnswers, PageResult answerKey) {
+        String data = "";
+        ArrayList<String> correctAnswers = answerKey.getAnswers();
+        ArrayList<String> questions = studentAnswers[0].getAnswers();
+        for (int i = 1; i < questions.size() + 1; i++) {
+            int count = 0;
+            for (PageResult student : studentAnswers) {
+                ArrayList<String> answers = student.getAnswers();
+                if (answers.get(i - 1).equals(correctAnswers.get(i - 1))) {
                     count++;
                 }
             }
-            data+="For question "+i+" the score for the students is "+count+"/"+studentAnswers.length+"\n";
+            data += "For question " + i + " the score for the students is " + count + "/" + studentAnswers.length + "\n";
         }
 
-return data;
+        return data;
     }
 
-public static String gradeTest(PageResult[] studentAnswers, PageResult answerKey){
-        String data="";
-        ArrayList<String>correctAnswers=answerKey.getAnswers();
-    int studentNum=1;
-    for (PageResult student:studentAnswers){
-        int correctNum=0;
-        data+="Grading for student "+studentNum+": \n";
-        ArrayList<String>answers=student.getAnswers();
-        for (int i = 1; i < answers.size()+1; i++) {
-            if( answers.get(i-1).equals(correctAnswers.get(i-1))){
-                data+="Question "+i+" is correct.\n";
-                correctNum++;
-            }else{
-                data+="Question "+i+" is wrong.\n";
+    public static String gradeTest(PageResult[] studentAnswers, PageResult answerKey) {
+        String data = "";
+        ArrayList<String> correctAnswers = answerKey.getAnswers();
+        int studentNum = 1;
+        for (PageResult student : studentAnswers) {
+            int correctNum = 0;
+            data += "Grading for student " + studentNum + ": \n";
+            ArrayList<String> answers = student.getAnswers();
+            for (int i = 1; i < answers.size() + 1; i++) {
+                if (answers.get(i - 1).equals(correctAnswers.get(i - 1))) {
+                    data += "Question " + i + " is correct.\n";
+                    correctNum++;
+                } else {
+                    data += "Question " + i + " is wrong.\n";
+                }
             }
+            data += "Student " + studentNum + " got " + correctNum + " answers correct out of " + answers.size() + " questions.\n";
+            studentNum++;
         }
-        data+="Student "+studentNum+" got "+correctNum+" answers correct out of "+answers.size()+" questions.\n";
-        studentNum++;
+        return data;
     }
-return data;
-}
-        public static PageResult setFilter(int pageNum, String pathToPdf){
-            PImage in = PDFHelper.getPageImage(pathToPdf,pageNum);
-            DImage img = new DImage(in);
-            System.out.println("Running filter on page " +pageNum);
-            findAnswersFilter filter = new findAnswersFilter();
-            filter.processImage(img);
-            PageResult result=filter.getResult();
-            return result;
-        }
+
+    public static PageResult setFilter(int pageNum, String pathToPdf) {
+        PImage in = PDFHelper.getPageImage(pathToPdf, pageNum);
+        DImage img = new DImage(in);
+        System.out.println("Running filter on page " + pageNum);
+        findAnswersFilter filter = new findAnswersFilter();
+        filter.processImage(img);
+        PageResult result = filter.getResult();
+        return result;
+    }
 
 
 
@@ -88,8 +89,6 @@ return data;
         (3).  Create a DImage from each page and process its pixels
         (4).  Output 2 csv files
          */
-
-
 
 
     private static String fileChooser() {
--- src/findAnswersFilter.java
+++ src/findAnswersFilter.java
@@ -19,34 +19,33 @@ public class findAnswersFilter implements PixelFilter {
     private static int numQuestionsPerPage=25;
     */
     //constant values for second PDF(3 pages)
-    private static int firstQuestionX=87;
-    private static int firstQuestionY=91;
-    private static int xDist=9999;
-    private static int yDist=48;
-    private static int answerDist=24;
-    private static int numQuestionsPer=19;
-//constant values for student id in second PDF
-
+    private static int firstQuestionX = 87;
+    private static int firstQuestionY = 91;
+    private static int xDist = 9999;
+    private static int yDist = 48;
+    private static int answerDist = 24;
+    private static int numQuestionsPer = 19;
 
 
     private String data;
     private String id;
-    ArrayList<String>answerDoc=new ArrayList<>();
+    ArrayList<String> answerDoc = new ArrayList<>();
 
     public findAnswersFilter() {
         System.out.println("Filter running...");
-        data="";
-        id="";
     }
-    public String getData(){
+
+    public String getData() {
         return data;
     }
-    public PageResult getResult(){
-        PageResult p=new PageResult();
+
+    public PageResult getResult() {
+        PageResult p = new PageResult();
         p.setStudentId(id);
         p.addAnswers(answerDoc);
         return p;
     }
+
     public ArrayList<String> getAnswerDoc() {
         return answerDoc;
     }
@@ -54,63 +53,62 @@ public class findAnswersFilter implements PixelFilter {
     @Override
     public DImage processImage(DImage img) {
         short[][] grid = img.getBWPixelGrid();
-        System.out.println("Image is " + grid.length + " by "+ grid[0].length);
-        //id=findAnswer()
+        System.out.println("Image is " + grid.length + " by " + grid[0].length);
 
-        ArrayList<Point> questions=findQuestions(img);
-        for (int i = 1; i < questions.size()+1; i++) {
-            String answer=findAnswer(questions.get(i-1), grid);
-            System.out.println("the answer to question "+i+" is "+answer);
-           answerDoc.add(answer);
+        ArrayList<Point> questions = findQuestions(img);
+        for (int i = 1; i < questions.size() + 1; i++) {
+            String answer = findAnswer(questions.get(i - 1), grid);
+            System.out.println("the answer to question " + i + " is " + answer);
+            answerDoc.add(answer);
         }
         return img;
     }
 
-    public static ArrayList<Point> findQuestions( DImage img){
-        ArrayList<Point>points=new ArrayList<>();
-        Point firstQuestion=new Point(firstQuestionX,firstQuestionY);
-        for (int x = firstQuestionX; x < img.getWidth(); x+=xDist) {
-            for (int y = firstQuestionY; y <firstQuestionY+yDist*numQuestionsPer ; y+=yDist) {
-                Point question=new Point(x,y);
+    public static ArrayList<Point> findQuestions(DImage img) {
+        ArrayList<Point> points = new ArrayList<>();
+        for (int x = firstQuestionX; x < img.getWidth(); x += xDist) {
+            for (int y = firstQuestionY; y < firstQuestionY + yDist * numQuestionsPer; y += yDist) {
+                Point question = new Point(x, y);
                 points.add(question);
             }
         }
         return points;
     }
-    private static String findAnswer(Point question, short[][]grid){
 
-        int a1count=getBlackPixels(question.getX(), question.getY(), grid );
-        int a2count=getBlackPixels(question.getX()+answerDist,question.getY(),grid);
-        int a3count=getBlackPixels(question.getX()+(2*answerDist),question.getY(),grid);
-        int a4count=getBlackPixels(question.getX()+(3*answerDist),question.getY(),grid);
-        int a5count=getBlackPixels(question.getX()+(4*answerDist),question.getY(),grid);
-        System.out.println(a1count+" "+a2count+" "+a3count+" "+a4count+" "+a5count);
-        if(a1count>a2count&&a1count>a3count&&a1count>a4count&&a1count>a5count){
+    private static String findAnswer(Point question, short[][] grid) {
+
+        int a1count = getBlackPixels(question.getX(), question.getY(), grid);
+        int a2count = getBlackPixels(question.getX() + answerDist, question.getY(), grid);
+        int a3count = getBlackPixels(question.getX() + (2 * answerDist), question.getY(), grid);
+        int a4count = getBlackPixels(question.getX() + (3 * answerDist), question.getY(), grid);
+        int a5count = getBlackPixels(question.getX() + (4 * answerDist), question.getY(), grid);
+        System.out.println(a1count + " " + a2count + " " + a3count + " " + a4count + " " + a5count);
+        if (a1count > a2count && a1count > a3count && a1count > a4count && a1count > a5count) {
             return "answer 1";
-        }else if(a2count>a1count&&a2count>a3count&&a2count>a4count&&a2count>a5count){
+        } else if (a2count > a1count && a2count > a3count && a2count > a4count && a2count > a5count) {
             return "answer 2";
-        }else if(a3count>a1count&&a3count>a2count&&a3count>a4count&&a3count>a5count){
+        } else if (a3count > a1count && a3count > a2count && a3count > a4count && a3count > a5count) {
             return "answer 3";
-        }else if(a4count>a1count&&a4count>a3count&&a4count>a3count&&a4count>a5count){
+        } else if (a4count > a1count && a4count > a2count && a4count > a3count && a4count > a5count) {
             return "answer 4";
-        }else {
-            return  "answer 5";
+        } else {
+            return "answer 5";
         }
     }
 
-    private static int getBlackPixels( double x, double y, short[][]grid ){
-        int count=0;
-        for (int j = (int) y; j <y+yDist ; j++) {
-            for (int k = (int) x; k <x+answerDist ; k++) {
-                if(grid[j][k]<10){
-                    count+=5;
-                }else if(grid[j][k]<50) {
+    private static int getBlackPixels(double x, double y, short[][] grid) {
+        int count = 0;
+        for (int j = (int) y; j < y + yDist; j++) {
+            for (int k = (int) x; k < x + answerDist; k++) {
+                if (grid[j][k] < 20) {
+                    count += 5;
+                } else if (grid[j][k] < 50) {
                     count += 4;
-                }else if(grid[j][k]<100){
-                    count+=3;
-                }else if(grid[j][k]<150){
-                    count+=2;
-                }else if (grid[j][k]<200){
+                } else if (grid[j][k] < 100) {
+                    count += 3;
+                } else if (grid[j][k] < 150) {
+                    count += 2;
+                } else if (grid[j][k] < 200) {
                     count++;
                 }
             }
